0b23904dfd9e40d9b8fc3c322eb30f6cb6af0255
Marzio95 <marziodellarocca14@gmail.com>
2022-03-24 19:36:08 +0100

    implemento un tasto select

--- src/components/HeaderDischi.vue
+++ src/components/HeaderDischi.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="bkg_cont text-white">
+      <div
+        class="b_t text-white d-flex justify-content-between align-items-center p-3"
+      >
         <h1>SPOTIFY</h1>
+
+        <select class="rounded" name="generi" id="generi">
+          <option default value="general">General</option>
+          <option value="rock">Rock</option>
+          <option value="jazz">Jazz</option>
+          <option value="metal">Metal</option>
+          <option value="pop">Pop</option>
+        </select>
       </div>
     </div>
   </div>
@@ -15,7 +25,11 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.bkg_cont {
+.b_t {
   background-color: #2e3a46;
+}
+select {
+  height: 2.3rem;
+  width: 5.5rem;
 }
 </style>
