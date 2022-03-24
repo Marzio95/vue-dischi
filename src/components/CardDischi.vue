969366e4c4a2a80e8b943b60db51ad775b17fff8
Marzio95 <marziodellarocca14@gmail.com>
2022-03-24 16:42:26 +0100

    abbozzo il layout senza l api

---
+++ src/components/CardDischi.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card col-lg-2">
+    <img class="img" :src="cardSrc" :alt="cardText" />
+    <h2>{{ cardText }}</h2>
+    <h3>{{ cardAuthor }}</h3>
+    <h3>{{ cardYear }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardDischi",
+  props: {
+    cardSrc: String,
+    cardText: String,
+    cardAuthor: String,
+    cardYear: String,
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/components/HeaderDischi.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="bg-dark">header</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderDischi",
+};
+</script>
+
+<style scoped lang="scss"></style>
